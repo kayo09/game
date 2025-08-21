2985f6780a0e9e4c38e4dc21dda65ebfe8f0f25d
kayo09 <68217041+kayo09@users.noreply.github.com>
2025-08-21 22:50:21 +0530

    small changes in the right direction

--- engines/Psyche.py
+++ engines/Psyche.py
@@ -1,30 +1,37 @@
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 #Implementaton of Hardware abstraction layer, POSIX
 #Questions to ask user 
 #Note: lacks better accessibility yet
 class Psyche:
-    def __init__(self, choices: List[Tuple])-> None:
+    def __init__(self, choices: Optional[List[Tuple]] = None) -> None:
+        if choices is None: 
+            choices = []
         self.choices = choices
-        pass
 
     def anal_kind(self,):
-        #logic to analyze kind
-        pass
+        #logic to analyze kind and create a point system to generate an avatar
+        kind = self.choices[0]
+        
 
     def diff_senses(self,):
         #logic to analyze senses, treating sense count as difficulty classifier for game
         pass
 
     #creates a choices object
-    def hi(self):
+    def hi(self) -> None:
         print("You're about to make a big CHOICE, chose wisely! or don't it's just some garbage code ;)")
-        print("What kind of human are you?")
-        kind = str(input())
-        print("How many tactile senses do you poses?")
-        senses = int(input())
-        print("Can i classify you while you evolve?")
-        choice_0 = bool(input())
+        while True:
+            kind = str(input("What kind of human are you? (max 280 chars)\n"))
+            if len(kind)<=280:
+                break
+            print("⚠ Too BIG! Try again")
+        senses = int(input("How many tactile senses do you poses?"))
+        choice_0 = bool(input("Can i classify you while you evolve?"))
+    
         self.choices = (kind, senses, choice_0)
 
-
+if __name__ == "__main__":
+    obj = Psyche()
+    obj.hi()
+    print(obj.choices)
